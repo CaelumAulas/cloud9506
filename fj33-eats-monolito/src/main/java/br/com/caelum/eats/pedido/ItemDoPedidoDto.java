a738d9db69383fc2422d47a0ddc28b8766f4f6ab
alefhsousa <alefh.sousa@gmail.com>
2021-03-22 19:48:25 -0300

    adicionando primeira versão do código monolito

---
+++ fj33-eats-monolito/src/main/java/br/com/caelum/eats/pedido/ItemDoPedidoDto.java
@@ -0,0 +1,23 @@
+package br.com.caelum.eats.pedido;
+
+import br.com.caelum.eats.dto.ItemDoCardapioDto;
+import br.com.caelum.eats.pedido.ItemDoPedido;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDoPedidoDto {
+
+	private Long id;
+	private Integer quantidade;
+	private String observacao;
+	private ItemDoCardapioDto itemDoCardapio;
+
+	public ItemDoPedidoDto(ItemDoPedido item) {
+		this(item.getId(), item.getQuantidade(), item.getObservacao(), new ItemDoCardapioDto(item.getItemDoCardapio()));
+	}
+
+}
